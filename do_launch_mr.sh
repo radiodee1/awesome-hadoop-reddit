1abe6d755dfd7b37588651c441327102fba7237e
David Liebman <radiodee1@gmail.com>
2018-09-02 10:28:04 -0400

    seems to run

--- do_launch_mr.sh
+++ do_launch_mr.sh
@@ -4,5 +4,7 @@ export HADOOP_HOME=/usr/local/hadoop
 
 STREAM=$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar
 
-$HADOOP_HOME/bin/hadoop jar $STREAM -input /opt/hadoop/input_dir -output /opt/hadoop/output_dir -mapper  ./mapper.py -reducer  ./reducer.py
+$HADOOP_HOME/bin/hadoop dfs -rm -r ~/hadoop-output
+
+$HADOOP_HOME/bin/hadoop jar $STREAM -input /home/hadoop/hadoop-input -output /home/hadoop/hadoop-output -mapper  ./mapper.py -reducer  ./reducer.py
 
