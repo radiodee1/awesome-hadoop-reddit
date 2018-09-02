458210a7b30fadfcb3960a0ac9b9f1d9b38cc069
David Liebman <radiodee1@gmail.com>
2018-09-02 16:50:04 -0400

    mapper and reducer

--- reducer.py
+++ reducer.py
@@ -17,18 +17,18 @@ def main(separator='\t'):
     #   current_word - string containing a word (the key)
     #   group - iterator yielding all ["&lt;current_word&gt;", "&lt;count&gt;"] items
 
+    try:
+        for current_words, group in groupby(data, itemgetter(0,1,2)):
 
-    for current_words, group in groupby(data, itemgetter(0,1,2)):
-        
-        try:
-            #total_count = sum(int(count) for current_words, count in group)
-            total_count = sum(int(current_words[2]) for current_words in group)
-            print ("%s%s%s%s%d" % (current_words[0], separator, current_words[1], separator, total_count))
-        except ValueError:
-            # count was not a number, so silently discard this item
-            pass
-        except IndexError:
-            pass
+            try:
+                #total_count = sum(int(count) for current_words, count in group)
+                total_count = sum(int(current_words[2]) for current_words in group)
+                print ("%s%s%s%s%d" % (current_words[0], separator, current_words[1], separator, total_count))
+            except ValueError:
+                # count was not a number, so silently discard this item
+                pass
+    except IndexError:
+        pass
 
 if __name__ == "__main__":
     main()
