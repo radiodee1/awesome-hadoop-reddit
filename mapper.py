3e2058ac2200aa23dc69fbb1b7893aa10120b634
David Liebman <radiodee1@gmail.com>
2018-09-03 17:40:59 -0400

    mapper

--- mapper.py
+++ mapper.py
@@ -4,27 +4,40 @@
 import sys
 import re
 import json
+#from string import punctuation
+
+EMPTY = '--'
 
 
 def read_input(file):
+    list = []
     for line in file:
-        # split the line into words
-        row = json.loads(line)
 
-        body = row['body']
+        try:
+            row = json.loads(line)
+        except:
+            row['body'] = ''
+            pass
 
-        line = body
+        line = row['body']
 
-        yield re.split(r'[.]|[\r]', line)
+        #line = re.sub(r'(?<=['+punctuation+'])\s+(?=[A-Z])', '\n', line)
+        #return line.split('\n')
+        yield re.split(r'[.]|[\r]|[?]|[!]', line)
+
 
 def main(separator='\t'):
-    last_sentence = ''
+    last_sentence = EMPTY
 
     data = read_input(sys.stdin)
 
     for lines in data:
 
         for sentence in lines:
+            if last_sentence == EMPTY or len(sentence.strip()) == 0 or len(last_sentence.strip()) == 0:
+                last_sentence = sentence
+                continue
+
             sentence = sentence.strip()
             print ('%s%s%s%s%d' % (last_sentence, separator, sentence, separator, 1))
             last_sentence = sentence
