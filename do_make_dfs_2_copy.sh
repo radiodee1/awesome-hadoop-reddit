5f8fc4aa3149621191767057a359cbeef45d1f43
David Liebman <radiodee1@gmail.com>
2018-09-06 16:37:38 -0400

    echo stmt

--- do_make_dfs_2_copy.sh
+++ do_make_dfs_2_copy.sh
@@ -1,6 +1,6 @@
+echo "This may take some time!"
 
 $HADOOP_HOME/bin/hadoop dfs -mkdir -p ~/hadoop-input 
-$HADOOP_HOME/bin/hadoop dfs -copyFromLocal ./data/*  ~/hadoop-input
-#$HADOOP_HOME/bin/hadoop dfs -put ./data/*.txt ~/hadoop-input
+$HADOOP_HOME/bin/hadoop dfs -copyFromLocal ~/hadoop-input/*  ~/hadoop-input/.
 
 
