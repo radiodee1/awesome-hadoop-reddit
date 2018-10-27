80731f03dcd5ee61b8d70aabddebb56671acb4f2
David Liebman <david.c.liebman@gmail.com>
2018-10-27 17:45:25 -0400

    job map and job reduce.

---
+++ job_test.py
@@ -0,0 +1,30 @@
+import job_mapper
+import job_reducer
+
+import mrjob
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+class JobTest(MRJob):
+
+    INPUT_PROTOCOL  = mrjob.protocol.TextValueProtocol
+    OUTPUT_PROTOCOL = mrjob.protocol.TextProtocol
+    INTERNAL_PROTOCOL = mrjob.protocol.TextProtocol
+
+    def steps(self):
+        return [
+            MRStep(mapper=job_mapper.main,
+                   #combiner=self.combiner_count_words,
+                   reducer=job_reducer.main),
+            #MRStep(reducer=self.reducer_find_max_word)
+        ]
+
+
+
+
+if __name__ == '__main__':
+    JobTest.run()
