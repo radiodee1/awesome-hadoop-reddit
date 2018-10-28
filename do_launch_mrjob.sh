13b8200cb327855d57737c59d7fa137b7894acc3
David Liebman <radiodee1@gmail.com>
2018-10-28 12:12:06 -0400

    run file

---
+++ do_launch_mrjob.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export HADOOP_HOME=/usr/local/hadoop
+
+#STREAM=$HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-3.1.1.jar
+
+$HADOOP_HOME/bin/hadoop dfs -rm -r ~/hadoop-output
+
+#$HADOOP_HOME/bin/hadoop jar $STREAM -input /home/hadoop/hadoop-input -output /home/hadoop/hadoop-output -mapper  ./mapper.py -reducer  ./reducer.py
+
+python3.6 job_test.py -r hadoop < RC_IN.txt > out.txt
+
+
