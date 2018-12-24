c10eec6e1acf276e0cdfb43a3a27b35ae49a9b5e
David Liebman <david.c.liebman@gmail.com>
2018-12-24 14:00:09 -0500

    stagger output

--- do_split.py
+++ do_split.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     parser.add_argument('--mode', help='"test", "train", or "valid" - "test.big" and "test.babi" allowed (default = "train")')
     parser.add_argument('--zip-file', help='name of zip file to archive to')
     parser.add_argument('--autoencode', help='setup files for autoencode operation.', action='store_true')
+    parser.add_argument('--stagger', help='stagger input for P.O.S.-style training.', action='store_true')
 
     args = parser.parse_args()
     args = vars(args)
@@ -44,6 +45,7 @@ if __name__ == '__main__':
     arg_zip = None #'train-files'
     arg_filelist = []
     arg_autoencode = False
+    arg_stagger = False
 
     arg_mode = hparams['train_name']
 
@@ -97,6 +99,9 @@ if __name__ == '__main__':
     if args['autoencode'] == True:
         arg_autoencode = True
 
+    if args['stagger'] == True:
+        arg_stagger = True
+
     arg_destination = arg_filename + '.output.txt'
 
     if not arg_processed:
@@ -145,24 +150,73 @@ if __name__ == '__main__':
                 ques = open(arg_destination_question, 'w')
                 arg_filelist.append(arg_destination_question.split('/')[-1])
 
+            if arg_stagger:
+                print('stagger output.')
+
             for line in z:
                 save = ''
                 if num >= arg_start and (arg_length == 0 or num < arg_start + arg_length):
                     line = line.split('\t')
-                    src.write(line[0])
-                    save = line[0][:]
-                    if not line[0].endswith('\n'):
-                        src.write('\n')
-                    if arg_triplets:
-                        if arg_question is not None and arg_question != '':
-                            line[0] = arg_question
-                        ques.write(line[0])
+
+                    if not arg_stagger:
+
+                        src.write(line[0])
+                        save = line[0][:]
                         if not line[0].endswith('\n'):
-                            ques.write('\n')
-                    if arg_autoencode: line[1] = save #line[0]
-                    tgt.write(line[1])
-                    if not line[1].endswith('\n'):
-                        tgt.write('\n')
+                            src.write('\n')
+                        if arg_triplets:
+                            if arg_question is not None and arg_question != '':
+                                line[0] = arg_question
+                            ques.write(line[0])
+                            if not line[0].endswith('\n'):
+                                ques.write('\n')
+                        if arg_autoencode: line[1] = save #line[0]
+                        tgt.write(line[1])
+                        if not line[1].endswith('\n'):
+                            tgt.write('\n')
+
+                    else:
+                        src_stagger = ''
+                        tgt_stagger = ''
+                        ques_stagger = ''
+                        save = line[0][:]
+                        save_lst = save.split(' ')
+                        tgt_lst = line[1].split(' ')
+                        for i in range(len(save_lst)):
+
+                            word = save_lst[i]
+
+                            if i < len(tgt_lst): ii = i
+                            else: ii = len(tgt_lst) - 1
+
+                            ques_stagger = word
+                            if len(src_stagger) > 0: src_stagger += ' '
+
+                            src_stagger += word
+
+                            src.write(src_stagger)
+                            save = src_stagger
+                            if not src_stagger.endswith('\n'):
+                                src.write('\n')
+                            if arg_triplets:
+                                if arg_question is not None and arg_question != '':
+                                    ques_stagger = arg_question
+                                ques.write(ques_stagger)
+                                if not ques_stagger.endswith('\n'):
+                                    ques.write('\n')
+                            tgt_stagger = tgt_lst[ii]
+                            if arg_autoencode: tgt_stagger = save  # line[0]
+                            tgt.write(tgt_stagger)
+                            if not tgt_stagger.endswith('\n'):
+                                tgt.write('\n')
+
+                            if i != 0: num += 1
+
+                            if arg_length != 0 and num > arg_start + arg_length:
+                                print('early stop')
+                                break
+
+                        pass
 
                 if arg_length != 0 and num > arg_start + arg_length:
                     print('early stop')
